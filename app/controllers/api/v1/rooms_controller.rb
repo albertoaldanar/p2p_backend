ec750199387fde6a8712f5cd486cc6333fbc62ef
Alberto Aldana <albertoaldanar@hotmail.com>
2018-02-12 21:08:52 -0700

    Rooms controller api´s ready

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -5,5 +5,29 @@ class API::V1::roomsController < ApplicationController
     else
       rooms = Room.where(active: true)
     end
-  end
+
+    if !params[:start_date].blank? && !params[:end_date].blank?
+      start_date = DateTime.parse(params[:start_date])
+      end_date = DateTime.parse(params[:end_date])
+      rooms = rooms.select { |room|
+          #Checar cuantos cuartos se empalman con las fechas de mi reservacion.
+          reservations = Reservation.where(
+            "room_id = ? AND (start_date <= ? AND end_date >=?) AND status = ?",
+            room.id, end_date, start_date, 1
+          ).count
+          #Checar si hay algun dia no disponible en la fecha de reservación.
+          calendars = Calendar.where(
+            "room_id = ? AND status = ? AND day BETWEEN ? AND ?",
+            room.id, 1, start_date, end_date
+          ).count
+
+          reservations == 0 && calendars == 0
+       }
+    end
+
+    render json: {
+      rooms: rooms.map {|room| room.attributes.merge(image:room.cover_photo("medium"), instant: room.instant != "Request")},
+      is_success: true
+    },stauts: :ok
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: {format: :json} do
+  namespace :api, defautls: {format: :json} do
     namespace :v1 do
       get '/logout' => 'users#logout'
       post '/facebook' => 'users#facebook'
       get '/user_count' => 'users#all_users'
+      resources :rooms
     end
   end
 end
