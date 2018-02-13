a9b4217b3149976f07c613cd4def0dde193f6aab
Alberto Aldana <albertoaldanar@hotmail.com>
2018-02-12 23:54:03 -0700

    Serializer for api show and controller ready

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -30,4 +30,40 @@ class API::V1::roomsController < ApplicationController
       is_success: true
     },stauts: :ok
 
+  end
+
+  def show
+    room = Room.find(params[:id])
+    today = Date.today
+
+    reservations = Reservation.where(
+      "room_id = ? AND (start_date >= ? AND end_date >= ?) AND status = ?",
+      room.id, today, today, 1
+    )
+
+    unavaliable_dates = reservations.map{|res|
+      (res[:start_date].to_datetime...res[:end_date].to_datetime).map {|day|
+        day.strftime("%Y-%M-%D")
+      }.flatten.to_set
+    }
+
+    calendars = Calendar.where(
+      "room_id = ? AND status = ? AND day> = ?",
+      room.id, 1, today
+    ).pluck(:day).map(&:to_datetime).map {|day| day.strftime("%Y-%M-%D")}.flatten.to_set
+
+    unavaliable_dates.merge calendars
+
+    if !room.nil?
+      room_serializer = RoomSerializer.new(
+        room,
+        image: room.cover_photo("medium"),
+        avatar_url: room.user.image,
+        unavaliable_dates: unavaliable_dates
+      )
+      render json: {room: room_serializer, is_success: true}, status: :ok
+    else
+      render json: {error: "Invalid ID", is_success: false}, status: 422
+    end
+  end
 end
